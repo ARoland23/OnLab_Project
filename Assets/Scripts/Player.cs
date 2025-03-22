324dc0efd159d978be7c190a090dee1103868478
ARoland23 <23koromiroland@gmail.com>
2025-03-22 14:44:11 +0100

    Player movement implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]    private float moveSpeed = 1.0f;
+    [SerializeField]    private GameInput gameInput;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementNormalVector();
+        Vector3 moveDir = new Vector3(inputVector.x, inputVector.y);
+
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+        Debug.Log(inputVector);
+
+    }
+}
